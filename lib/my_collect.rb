0a52a350bc158c5a1c176d16bc19361f613f3c2f
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-18 17:58:29 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,19 @@ def my_collect(array)
   empty = []
   i = 0 
   while i < array.length 
- empty << yield (array[i])
+ empty << yield(array[i])
 i = i + 1
 end 
 empty
 end 
+
+
+def my_collect(array)
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
+  end
+  collect
+end
